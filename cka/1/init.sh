14439eb5d337f4620b311a7168bdbfb2fac6506f
k1m0ch1 <yahya.kimochi@gmail.com>
2025-06-13 23:32:27 +0700

    14 minutes to run all of this wtf

--- cka/1/init.sh
+++ cka/1/init.sh
@@ -12,6 +12,9 @@ CLIENT_EMAIL=$(jq -r '.client_email' "$KEY_FILE")
 # Step 3: Extract project ID from email (after '@' and before '.iam')
 PROJECT_ID=$(echo "$CLIENT_EMAIL" | sed -E 's/.*@(.*)\.iam.*/\1/')
 
+# Update gcp.yml with the extracted PROJECT_ID
+sed -i "s/^projects:.*/projects:\n  - $PROJECT_ID/" gcp.yml
+
 # Step 4: Set gcloud project
 gcloud config set project "$PROJECT_ID"
 
@@ -28,3 +31,19 @@ gcloud compute ssh c1-cp1 --command="uname -a"
 gcloud compute ssh c1-node1 --command="uname -a"
 gcloud compute ssh c1-node2 --command="uname -a"
 gcloud compute ssh c1-node3 --command="uname -a"
+
+export ANSIBLE_SSH_COMMON_ARGS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+
+ansible c1-cp1 -i gcp.yml -m ping --private-key ~/.ssh/google_compute_engine
+ansible c1-node1 -i gcp.yml -m ping --private-key ~/.ssh/google_compute_engine
+ansible c1-node2 -i gcp.yml -m ping --private-key ~/.ssh/google_compute_engine
+ansible c1-node3 -i gcp.yml -m ping --private-key ~/.ssh/google_compute_engine
+
+ansible-playbook -i gcp.yml disable_swap.yml --private-key ~/.ssh/google_compute_engine
+
+gcloud compute instances list --format="value(name,zone)" | while read name zone; do
+echo "Restarting $name in $zone"
+  gcloud compute instances reset "$name" --zone="$zone"
+done
+
+ansible-playbook -i gcp.yml containerd.yml --private-key ~/.ssh/google_compute_engine
